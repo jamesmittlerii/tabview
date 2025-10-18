7f3acef0ff69af4987a6a1e1a5f4fff7d89d722d
James Mittler II <jamesmittlerii@my.smccd.edu>
2025-10-18 10:28:14 -0400

    Initial Commit

---
+++ tabview/tabviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabviewApp.swift
+//  tabview
+//
+//  Created by cisstudent on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct tabviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
